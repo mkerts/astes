b0f60d9f9bbc3223a318fa630f01124587828e3e
mkerts <kursate277@gmail.com>
2026-03-04 23:25:08 +0300

    Create User.js MongoDB schema file

---
+++ backend/src/models/User.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String },
+    bio: { type: String },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
